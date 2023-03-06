bed7c2c44957785e057b841a4935e9f6c0acab66
TucoBenedicto <82284883+TucoBenedicto@users.noreply.github.com>
2023-03-06 14:12:53 +0100

    update

--- js/utils/env/index.test.js
+++ js/utils/env/index.test.js
@@ -1,14 +1,18 @@
 import { isInTestEnv } from "./index";
 
 describe("isInTestEnv Unit Test Suites", () => {
-  it("should return true", () => {
+  it("should be in test env", () => {
     expect(isInTestEnv()).toBe(true);
   });
 
-  it("should return true", () => {
-    process.env.NODE_ENV === "dev";//on passe la variabiable sur "dev" pour la rendre fausse et que elle passe le teste , grace a ".not"
-    expect(isInTestEnv()).not.toBe(true);
+  it("should not be in test env", () => {
+    //on passe la variabiable sur "dev" pour la rendre fausse et que elle passe le teste , grace a ".not"
+    //"Process" est une variable liée a nodeJS
+    process.env.NODE_ENV === 'dev';
+    expect(isInTestEnv()).not.toBe(false);
   });
+
+  
 });
 
 //NOT
@@ -19,5 +23,4 @@ ce code teste que le meilleur goût de La Croix n'est pas coconut :
 test('la meilleure saveur n\'est pas coconut', () => {
   expect(bestLaCroixFlavor()).not.toBe('coconut');
 });
-
 */
